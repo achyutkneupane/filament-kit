45ae6a3f782f05b30566c596ca66489925aa6102
Achyut Neupane <achyutkneupane@gmail.com>
2025-08-11 20:28:18 +0545

    build: Unwanted files removed

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Enums\UserRole;
 use App\Models\User;
+
 use function Pest\Laravel\actingAs;
 
 it('redirects to login page when filament is requested being guest', function (): void {
@@ -14,18 +16,16 @@ it('redirects to login page when filament is requested being guest', function ()
 
 it('renders filament dashboard when logged in', function (): void {
     /** @var User $admin */
-    $admin = User::factory()->create(['role' => \App\Enums\UserRole::Admin]);
+    $admin = User::factory()->create(['role' => UserRole::Admin]);
     actingAs($admin);
 
     $response = $this->get('/');
     $response->assertOk();
 });
 
-
-
 it('returns 403 if user try to access filament dashboard', function (): void {
     /** @var User $user */
-    $user = User::factory()->create(['role' => \App\Enums\UserRole::User]);
+    $user = User::factory()->create(['role' => UserRole::User]);
     actingAs($user);
 
     $response = $this->get('/');
